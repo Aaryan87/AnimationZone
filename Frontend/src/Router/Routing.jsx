e209e44d830353396ae34b89cbb7bfa7a507f1f0
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2024-03-25 12:38:02 +0530

    Navbar and Home page

--- Frontend/src/Router/Routing.jsx
+++ Frontend/src/Router/Routing.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
 
 const Routing = () => {
   return (
     <BrowserRouter>
         <Navbar/>
         <Routes>
-            <Route path="/" element={'Home'}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={'About'} />
         </Routes>
     </BrowserRouter>
   )
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div></div>
+    <div className='header'>
+        <NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md' >
+            <p className='blue-gradient_text'>AZ</p>
+        </NavLink>
+        <nav className='flex text-lg gap-7 font-medium'>
+
+            <NavLink to='/storybook' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                StoryBooks
+            </NavLink>
+
+            <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                About
+            </NavLink>
+
+            <NavLink to='/login' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                Login
+            </NavLink>
+
+        </nav>
+    </div>
   )
 }
 
